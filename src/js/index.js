18cf43eb46f6ff17e79e86b3cc0404f07b95f1dc
hector27gab <147425846+hector27gab@users.noreply.github.com>
2023-12-02 03:33:53 +0000

    simple-counter-with-react

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,12 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import react from "react";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+
+setInterval(() => {
+    counter += 1;
+    ReactDOM.render(<Home seconds={counter}  />, document.querySelector("#app"));
+},1000)
